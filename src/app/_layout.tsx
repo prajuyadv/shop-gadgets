76cdadb2e198428eec171e70872e6bc0de529130
prajuyadv <patnamprajukumaryadav@gmail.com>
2025-09-21 18:20:16 -0500

    sep 21st named basic flow

---
+++ src/app/_layout.tsx
@@ -0,0 +1,28 @@
+import { Stack } from "expo-router";
+
+export default  function RotLayout(){
+return (
+    <Stack>
+        <Stack.Screen 
+            name='(shop)'   
+            options={{headerShown: false, title: 'Shop'}}
+        />
+         <Stack.Screen 
+            name='categories'   
+            options={{headerShown: true, title: 'Categories'}}
+        />
+         <Stack.Screen 
+            name='product'   
+            options={{headerShown: true, title: 'Product'}}
+        />
+         <Stack.Screen 
+            name='cart'   
+            options={{presentation: 'modal', title: 'Cart'}}
+        />
+         <Stack.Screen 
+            name='auth'   
+            options={{headerShown: true, title: 'Auth'}}
+        />
+    </Stack>
+)
+}
