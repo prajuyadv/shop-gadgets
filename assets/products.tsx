5bcfd2a8842f34240fd118b30d30fe70dabce299
prajuyadv <patnamprajukumaryadav@gmail.com>
2025-09-22 18:20:50 -0500

    sep 22nd Fixed Assets Mock Api  Response

---
+++ assets/products.tsx
@@ -0,0 +1,130 @@
+import { Product } from "./types/product"
+
+export const PRODUCTS: Product[] = [
+  {
+     id: 1,
+     title: "MacBook-pro-2024",
+     slug:"MacBook-pro-2024",
+     heroImage: require('../assets/images/mac-book-1.jpg'),
+     imagesUrl: [
+      require('../assets/images/mac-book-1.jpg'),
+      require('../assets/images/mac-book-2.jpg'),
+      require('../assets/images/mac-book-3.jpg'),
+     ],
+     price: 899.99,
+     category: {
+      imageUrl: require('../assets/images/mac-book-1.jpg'),
+      name: 'Laptops',
+      slug: 'laptops',
+     },
+     maxQuantity: 5,
+},
+{
+  id: 2,
+  title: "Dell XPS 13",
+  slug:"Dell XPS 13",
+  heroImage: require('../assets/images/dell-1.jpg'),
+  imagesUrl: [
+   require('../assets/images/dell-1.jpg'),
+   require('../assets/images/dell-2.jpg'),
+  ],
+  price: 899.99,
+  category: {
+   imageUrl: require('../assets/images/dell-1.jpg'),
+   name: 'Laptops',
+   slug: 'laptops',
+  },
+  maxQuantity: 5,
+},
+{
+  id: 1,
+  title: "MacBook-pro-2024",
+  slug:"MacBook-pro-2024",
+  heroImage: require('../assets/images/head-set-1.jpg'),
+  imagesUrl: [
+   require('../assets/images/head-set-1.jpg'),
+   require('../assets/images/head-set-2.jpg'),
+  ],
+  price: 899.99,
+  category: {
+   imageUrl: require('../assets/images/head-set-1.jpg'),
+   name: 'Laptops',
+   slug: 'laptops',
+  },
+  maxQuantity: 5,
+},
+{
+  id: 1,
+  title: "ps-5",
+  slug:"ps-5",
+  heroImage: require('../assets/images/ps-5-1.jpg'),
+  imagesUrl: [
+   require('../assets/images/ps-5-1.jpg'),
+   require('../assets/images/ps-5-2.jpg'),
+   require('../assets/images/ps-5-3.jpg'),
+  ],
+  price: 899.99,
+  category: {
+   imageUrl: require('../assets/images/ps-5-1.jpg'),
+   name: 'Laptops',
+   slug: 'laptops',
+  },
+  maxQuantity: 5,
+},
+{
+  id: 1,
+  title: "samsung",
+  slug:"samsung",
+  heroImage: require('../assets/images/samsung-1.jpg'),
+  imagesUrl: [
+   require('../assets/images/samsung-1.jpg'),
+   require('../assets/images/samsung-2.jpg'),
+  ],
+  price: 899.99,
+  category: {
+   imageUrl: require('../assets/images/samsung-1.jpg'),
+   name: 'Laptops',
+   slug: 'laptops',
+  },
+  maxQuantity: 5,
+},
+{
+  id: 1,
+  title: "nintendo-switch",
+  slug:"samsung",
+  heroImage: require('../assets/images/nintendo-switch-1.jpg'),
+  imagesUrl: [
+   require('../assets/images/nintendo-switch-1.jpg'),
+   require('../assets/images/nintendo-switch-2.jpg'),
+  ],
+  price: 899.99,
+  category: {
+   imageUrl: require('../assets/images/nintendo-switch-1.jpg'),
+   name: 'Laptops',
+   slug: 'laptops',
+  },
+  maxQuantity: 5,
+},
+{
+  id: 1,
+  title: "i-phone",
+  slug:"i-phone",
+  heroImage: require('../assets/images/i-phone-1.jpg'),
+  imagesUrl: [
+   require('../assets/images/i-phone-1.jpg'),
+   require('../assets/images/i-phone-2.jpg'),
+   require('../assets/images/i-phone-3.jpg'),
+  ],
+  price: 899.99,
+  category: {
+   imageUrl: require('../assets/images/i-phone-1.jpg'),
+   name: 'Laptops',
+   slug: 'laptops',
+  },
+  maxQuantity: 5,
+}
+]
+
+
+
+
--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,14 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { PRODUCTS } from "../../../assets/products";
 
 const Home = () => {
+  // console.log("PRODUCTS", PRODUCTS)
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+          data={PRODUCTS} 
+          renderItem={(item)=>(
+            <View>
+              <Text>{item.title}</Text>
+            </View> 
+          )}
+          keyExtractor={item => item.id.toString()} 
+          numColumns={2}
+          ListHeaderComponent={<Text>Products</Text>}
+          contentContainerStyle={styles.flatListContent}
+          columnWrapperStyle={styles.flatListColumn}
+          style={{ paddingHorizontal : 10, paddingVertical: 5}}
+       />
+
     </View>
-  )
-}
+  );
+};
 
 export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+flatListContent:{
+    paddingBottom:20,
+  },
+  flatListColumn:{
+    justifyContent:'space-between',
+  }
+
+});
